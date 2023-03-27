244a57ed4552a55b01aef10ab1db4b5e713adbca
wareeewaa <wareewaa@gmail.com>
2023-03-27 20:18:54 +0200

    gui base

---
+++ encryptdecrypt.py
@@ -0,0 +1,35 @@
+# import
+
+
+def encrypt(text, key, mode):
+    if mode == 1:
+        print("1")
+    elif mode == 2:
+        print("1")
+    elif mode == 3:
+        print("1")
+    elif mode == 4:
+        print("1")
+    elif mode == 5:
+        print("1")
+    elif mode == 6:
+        print("1")
+    encryptedText = text + key
+    return encryptedText
+
+
+def decrypt(text, key, mode):
+    if mode == 1:
+        print("2")
+    elif mode == 2:
+        print("2")
+    elif mode == 3:
+        print("2")
+    elif mode == 4:
+        print("2")
+    elif mode == 5:
+        print("2")
+    elif mode == 6:
+        print("2")
+    decryptedText = text+key
+    return decryptedText
--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import tkinter as tk
+from encryptdecrypt import encrypt, decrypt
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def open_window(mode):
+    window = tk.Toplevel()
+    window.geometry("400x250")
+    if mode == 1:
+        window.title("1")
+    elif mode == 2:
+        window.title("2")
+    elif mode == 3:
+        window.title("3")
+    elif mode == 4:
+        window.title("4")
+    elif mode == 5:
+        window.title("5")
+    elif mode == 6:
+        window.title("6")
+
+    text_label = tk.Label(window, text="Text:")
+    text_label.pack()
+    text_input = tk.Entry(window, width=30)
+    text_input.pack()
+    key_label = tk.Label(window, text="Key:")
+    key_label.pack()
+    key_input = tk.Entry(window, width=30)
+    key_input.pack()
+    output_label = tk.Label(window, text="Output:")
+    output_label.pack()
+    output = tk.Entry(window, width=30)
+    output.pack()
+
+    def encrypt_message():
+        text = text_input.get()
+        key = key_input.get()
+        encrypted_text = encrypt(text, key, mode)
+        output.delete(0, tk.END)
+        output.insert(0, encrypted_text)
+
+    def decrypt_message():
+        text = text_input.get()
+        key = key_input.get()
+        decrypted_text = decrypt(text, key, mode)
+        output.delete(0, tk.END)
+        output.insert(0, decrypted_text)
+
+    encrypt_button = tk.Button(window, text="Encrypt", width=10, command=encrypt_message)
+    encrypt_button.pack(side=tk.LEFT, padx=50)
+    decrypt_button = tk.Button(window, text="Decrypt", width=10, command=decrypt_message)
+    decrypt_button.pack(side=tk.RIGHT, padx=50)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+root = tk.Tk()
+root.title("Main Menu")
+root.geometry("400x300")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+button1 = tk.Button(root, text="Open Window 1", command=lambda: open_window(1))
+button1.pack(pady=10)
+
+button2 = tk.Button(root, text="Open Window 2", command=lambda: open_window(2))
+button2.pack(pady=10)
+
+button3 = tk.Button(root, text="Open Window 3", command=lambda: open_window(3))
+button3.pack(pady=10)
+
+button4 = tk.Button(root, text="Open Window 4", command=lambda: open_window(4))
+button4.pack(pady=10)
+
+button5 = tk.Button(root, text="Open Window 5", command=lambda: open_window(5))
+button5.pack(pady=10)
+
+button6 = tk.Button(root, text="Open Window 6", command=lambda: open_window(6))
+button6.pack(pady=10)
+
+
+root.mainloop()
