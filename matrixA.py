0b621d3c2da8588818c0347a80c67fb976b5f426
wareeewaa <wareewaa@gmail.com>
2023-03-27 23:40:12 +0200

    matrixA

---
+++ matrixA.py
@@ -0,0 +1,32 @@
+def matrix_a_encrypt(text, key):
+    cols = len(key)
+    rows = int(len(text)/cols) + 1
+    encryptedText = ""
+    for row in range(0, rows):
+        for number in key:
+            index = int(number) + row * cols - 1
+            if index < len(text):
+                encryptedText += text[index]
+    return encryptedText
+
+
+def matrix_a_decrypt(text, key):
+    cols = len(key)
+    rows = int(len(text) / cols) + 1
+    decryptedText = ""
+    count_index = 0
+    for row in range(0, rows):
+        fragment = ['', '', '', '']
+        for count, number in enumerate(key):
+            number_index = int(number) + row * cols - 1
+            if number_index >= len(text):
+                count_index -= 1
+            elif count_index < len(text):
+                fragment[int(number)-1] = text[count_index]
+            count_index += 1
+        decryptedText += ''.join(fragment)
+    return decryptedText
+
+
+print(matrix_a_encrypt("CRYPTOGRAPHYOSA", "3142"))
+print(matrix_a_decrypt("YCPRGTROHAYPAOS", "3142"))
