e36d55463d062da459c0ea0a8e3d56bfba4f87ef
wareeewaa <wareewaa@gmail.com>
2023-04-23 21:48:49 +0200

    stream cypher

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from encryptdecrypt import encrypt, decrypt
-
+from tkinter import filedialog
+from stream import stream_encrypt, stream_decrypt
 
 def open_window(mode):
     window = tk.Toplevel()
@@ -17,6 +18,8 @@ def open_window(mode):
         window.title("Cezar")
     elif mode == 6:
         window.title("Vigenere")
+    #elif mode == 7:
+    #    window.title("Stream")
 
     text_label = tk.Label(window, text="Text:")
     text_label.pack()
@@ -51,9 +54,46 @@ def open_window(mode):
     decrypt_button.pack(side=tk.RIGHT, padx=50)
 
 
+def stream_open_window(mode):
+    window = tk.Toplevel()
+    window.geometry("400x250")
+    window.title("Stream")
+
+    def save_file(text):
+        file_path = filedialog.asksaveasfilename(defaultextension='.txt')
+        with open(file_path, 'w', encoding="utf-8") as file:
+            file.write(text)
+
+    def load_file():
+        polynomial = polynomial_input.get()
+        if polynomial.isnumeric() and int(polynomial) > 1:
+            file_path = filedialog.askopenfilename()
+            with open(file_path, 'r', encoding="utf-8") as file:
+                text = file.read()
+                if mode == 1:
+                    encrypted_text = stream_encrypt(text, polynomial)
+                elif mode == 2:
+                    encrypted_text = stream_decrypt(text, polynomial)
+                save_file_button = tk.Button(window, text="Save file", width=25, command=lambda: save_file(encrypted_text))
+                save_file_button.pack(pady=10)
+        else:
+            tk.messagebox.showerror("Error", "Enter correct value")
+
+    if mode == 1:
+        polynomial_label = tk.Label(window, text="Enter a polynomial (Must be higher than 1)")
+    elif mode == 2:
+        polynomial_label = tk.Label(window, text="Enter the key")
+    polynomial_label.pack()
+
+    polynomial_input = tk.Entry(window, width=30)
+    polynomial_input.pack()
+    select_file_button = tk.Button(window, text="Select a file", width=10, command=lambda: load_file())
+    select_file_button.pack(pady=10)
+
+
 root = tk.Tk()
 root.title("Main Menu")
-root.geometry("400x300")
+root.geometry("400x400")
 
 
 button1 = tk.Button(root, text="Rail Fence", command=lambda: open_window(1))
@@ -74,5 +114,10 @@ button5.pack(pady=10)
 button6 = tk.Button(root, text="Vigenere", command=lambda: open_window(6))
 button6.pack(pady=10)
 
+button7 = tk.Button(root, text="Stream Encrypt", command=lambda: stream_open_window(1))
+button7.pack(pady=10)
+
+button8 = tk.Button(root, text="Stream Decrypt", command=lambda: stream_open_window(2))
+button8.pack(pady=10)
 
 root.mainloop()
---
+++ venv/stream.py
@@ -0,0 +1,42 @@
+def stream_encrypt(text, polynomial):
+    binary_text = ''.join(format(ord(letter), '08b') for letter in text)
+    key_length = len(binary_text)
+    encrypted_binary_text = ""
+    encrypted_text = ""
+    encrypted_letters = []
+    '''
+    wywolanie lsfr podajac potrzebna dlugosc klucza i wielomian)
+    key = lfsr(key_length, polynomial)
+    print(key)
+    '''
+
+    for count, digit in enumerate(binary_text):
+        encrypted_binary_text += str(int(digit) ^ 1) #zamien 1 na key[count]
+        #print(str(count) + "  |   " + digit)
+   # print(binary_text)
+    #print(encrypted_binary_text)
+
+    for i in range(0, len(encrypted_binary_text), 8):
+        encrypted_letters.append(encrypted_binary_text[i:i + 8])
+    encrypted_text = "".join([chr(int(letter, 2)) for letter in encrypted_letters])
+    print(encrypted_text)
+    return encrypted_text
+
+def stream_decrypt(text,key):
+    binary_text = ''.join(format(ord(letter), '08b') for letter in text)
+    decrypted_binary_text = ""
+    decrypted_text = ""
+    decrypted_letters = []
+    for count, digit in enumerate(binary_text):
+        decrypted_binary_text += str(int(digit) ^ 1)  # zamien 1 na key[count]
+        # print(str(count) + "  |   " + digit)
+    # print(binary_text)
+    # print(encrypted_binary_text)
+
+    for i in range(0, len(decrypted_binary_text), 8):
+        decrypted_letters.append(decrypted_binary_text[i:i + 8])
+    decrypted_text = "".join([chr(int(letter, 2)) for letter in decrypted_letters])
+    print(decrypted_text)
+    return decrypted_text
+
+stream_encrypt("DDDDD",5)
